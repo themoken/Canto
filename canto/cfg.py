073c73e3f3ae09a15dd914d4e95cbe6d72a3a26a
Jack Miller <jack@codezen.org>
2008-05-01 15:12:07 -0500

    Fix broken exception, ConfigError. Now config breakage prints good messages.

--- canto/cfg.py
+++ canto/cfg.py
@@ -21,8 +21,6 @@ import interface_draw
 import traceback
 
 class ConfigError(Exception):
-    def __init__(self, value):
-        self.value = value
     def __str__(self):
         return repr(self.value)
 
