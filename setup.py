cda5ddd9a62ce9612ba6364554da505de01e3610
Jack Miller <jack@codezen.org>
2008-08-29 15:28:24 -0500

    Update log/setup for 0.4.8

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from distutils.core import setup, Extension
 from distutils.command.install_data import install_data
 import os
 
-version = ['0','4','7']
-man_date = "23 August 2008"
+version = ['0','4','8']
+man_date = "29 August 2008"
 
 class Canto_install_data(install_data):
     def run(self):
