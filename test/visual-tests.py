6c22da9b5f4c7ee926926bd4035bd2eb12376699
Jack Miller <jack@codezen.org>
2008-12-16 20:51:45 -0600

    Fix visual tests (for Unicode internal update)

--- test/visual-tests.py
+++ test/visual-tests.py
@@ -48,9 +48,9 @@ class TestIDraw(TestCurses):
         self.row = 0
         
         self.ufp = {}
-        self.ufp["title"] = "Here, try an HTML entity on for size: &amp;"
+        self.ufp["title"] = u"Here, try an HTML entity on for size: &amp;"
         self.ufp["description"] =\
-"""Super long string description. With word wrapping and
+u"""Super long string description. With word wrapping and
 line breaks
 and slugified-huge-words-just-like-the-widecurses-test-has. Also
 let's toss in some HTML, just for shits.
@@ -74,14 +74,14 @@ let's toss in some HTML, just for shits.
 <strong>Bold text</strong><br />
 <em>Emphasis text</em>"""
 
-        self.ufp["link"] = "http://somelink"
-        self.ufp["canto_state"] = ["unread"]
+        self.ufp["link"] = u"http://somelink"
+        self.ufp["canto_state"] = [u"unread"]
 
         self.story = Story(self.ufp, None, self.renderer)
         self.story.idx = 0
         self.story.last = 1
 
-        self.tag = Tag()
+        self.tag = Tag(None)
 
     def l(self, x):
         bme = ((""," ",""),(""," ",""),(""," ",""))
@@ -95,33 +95,37 @@ class TestIDrawBlocks(TestIDraw):
 
         self.width = 20
         # Simple quote
-        self.l("%QThis is a quote%q")
-        self.l(" ")
+        self.l(u"%QThis is a quote%q")
+        self.l(u" ")
 
         # Indent across a single line, that will break
-        self.l("%IThis is uniformly indented broken line write.%i")
-        self.l(" ")
+        self.l(u"%IThis is uniformly indented broken line write.%i")
+        self.l(u" ")
 
         # Multi line quote with forced breaks.
-        self.l("%QThis is a")
-        self.l("multi-line")
-        self.l("quote%q")
-        self.l(" ")
+        self.l(u"%QThis is a")
+        self.l(u"multi-line")
+        self.l(u"quote%q")
+        self.l(u" ")
 
 class TestIDrawStory(TestIDraw):
     def runTest(self):
-        self.renderer.story(self.tag, self.story, self.row, self.height,\
+        self.renderer.story(None, self.tag, self.story, self.row, self.height,\
                 self.width, [self.screen])
 
 class TestIDrawReader(TestIDraw):
     def runTest(self):
-        self.renderer.reader(self.story, self.width, 1, self.screen)
+        self.renderer.reader(None, self.story, self.width, 1, self.screen)
 
 from canto.widecurse import core
 class TestWidecurse(TestCurses):
     def l(self, x, r=" ", e=""):
+        p = locale.getpreferredencoding()
         self.row += 1
-        return core(self.screen, self.row, 0, self.width, x, r, e)
+        return core(self.screen, self.row, 0, self.width,\
+                x.encode(p), 
+                r.encode(p), 
+                e.encode(p))
 
     def runTest(self):
         if self.width < 20:
@@ -129,60 +133,62 @@ class TestWidecurse(TestCurses):
 
         self.width = 20
         # Terminal styles
-        self.l("%8Normal output")
-        self.l("%BBold output%b")
-        self.l("%UUnderline output%u")
-        self.l("%SStandout output%s")
-        self.l("%RReverse output%r")
-        self.l("%DDim output%d")
+        self.l(u"%8Normal output")
+        self.l(u"%BBold output%b")
+        self.l(u"%UUnderline output%u")
+        self.l(u"%SStandout output%s")
+        self.l(u"%RReverse output%r")
+        self.l(u"%DDim output%d")
         
         # Color output
         for i in range(8):
             curses.init_pair(i + 1, i, 0)
-            ouput = "%" + str(i + 1) + str(i + 1) + "%0"
-            for off,attr in enumerate(["", "%B","%U","%S"]):
+            ouput = u"%" + unicode(i + 1) + unicode(i + 1) + u"%0"
+            for off,attr in enumerate([u"", u"%B",u"%U",u"%S"]):
                 core(self.screen, self.row, 0 + off, self.width, \
-                        attr + ouput + attr.lower(), " ", "")
+                        (attr + ouput +
+                            attr.lower()).encode(locale.getpreferredencoding())\
+                            , " ", "")
             self.row += 1
 
         # Basic Color layering
-        self.l("%B%11%22%33%44%55%04%03%02%01%b")
-        self.l("%8Cleanse the palette.")
+        self.l(u"%B%11%22%33%44%55%04%03%02%01%b")
+        self.l(u"%8Cleanse the palette.")
 
         # Multi-line Color layering and end escapes
-        self.l("%2 This is the color 2")
-        self.l("This is color 2, too%0")
-        self.l("But not this")
+        self.l(u"%2 This is the color 2")
+        self.l(u"This is color 2, too%0")
+        self.l(u"But not this")
 
         # Empty string with repeating . ending in <><>
-        self.l("", ".","<><>")
+        self.l(u"", u".",u"<><>")
 
         # String of exact screen width
-        self.l("12345678901234567890")
+        self.l(u"12345678901234567890")
 
         # String of exact screen witdth, after escapes
-        self.l("%21234567890%51234567890")
+        self.l(u"%21234567890%51234567890")
 
         # String too long
-        r = self.l("%812345678901234567890extra")
+        r = self.l(u"%812345678901234567890extra")
 
         # Left over should be "extra"
-        self.l("Left over: %s" % r)
+        self.l(u"Left over: %s" % r)
 
         # Overflow the built-in color memory
         # This must be updated if color memory is increased
-        self.l("%1z%2y%3x%4w%5v%6u%7t%8s%1r%2q%0a%0b%0c%0d%0e%0f%0g%0h%0i%0j")
-        self.l(" ")
+        self.l(u"%1z%2y%3x%4w%5v%6u%7t%8s%1r%2q%0a%0b%0c%0d%0e%0f%0g%0h%0i%0j")
+        self.l(u" ")
 
         # Test word wrap
-        s = "%8A bunch of really short words, but more than one line."
+        s = u"%8A bunch of really short words, but more than one line."
         while s:
             s = self.l(s)
 
-        self.l(" ")
+        self.l(u" ")
 
         # Test word breaking
-        s = "A-really-long-slugified-huge-word"
+        s = u"A-really-long-slugified-huge-word"
         while s:
             s = self.l(s)
 
@@ -190,24 +196,25 @@ from canto.canto_html import convert
 class TestCantoHTML(TestCase):
     def runTest(self):
         # Test list nesting
-        text, links = convert("<ul><li>Unordered</li><li>Some header text"
-                "<ol><li>Ordered</li><li>Also ordered</li></ol>"
-                "<li>Unordered, too</li></ul>")
+        text, links = convert(u"<ul><li>Unordered</li><li>Some header text"
+                u"<ol><li>Ordered</li><li>Also ordered</li></ol>"
+                u"<li>Unordered, too</li></ul>")
 
-        self.failUnless(text == "\n%I\n● Unordered\n● Some header text\n"\
-            "%I\n1.Ordered\n2.Also ordered%i\n\n● Unordered, too%i\n")
+        self.failUnless(text == u"\n%I\n\u25cf Unordered\n\u25cf "\
+                u"Some header text\n%I\n1.Ordered\n2.Also ordered%i"\
+                u"\n\n\u25cf Unordered, too%i\n")
 
         # Test LinkHandler
-        text, links =  convert("""<a href="test">Blahblah</a>""")
+        text, links =  convert(u"""<a href="test">Blahblah</a>""")
 
-        self.failUnless(links == [("%4Blahblah","test","browser")])
+        self.failUnless(links == [(u"%4Blahblah",u"test",u"browser")])
         
         # Test ImageHandler
-        text,links = convert("""<img src="myimage.jpg" />
+        text,links = convert(u"""<img src="myimage.jpg" />
                             <img src="otherimage.jpg" alt="Sexy" />""")
 
-        self.failUnless(links == [("image","myimage.jpg","image"),\
-                ("Sexy","otherimage.jpg","image")])
+        self.failUnless(links == [(u"image",u"myimage.jpg",u"image"),\
+                (u"Sexy",u"otherimage.jpg",u"image")])
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
