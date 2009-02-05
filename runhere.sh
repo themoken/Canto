56c0f8b358f02a7915af84ad5f490eb3827e72d6
Jack Miller <jack@codezen.org>
2009-02-05 23:12:46 +0000

    Detect 64-bit in runhere.sh

--- runhere.sh
+++ runhere.sh
@@ -10,7 +10,14 @@ python setup.py install --prefix=$PWD/root
 
 PYVER=`python -c "import sys; print sys.version[:3]"`
 
-export PYTHONPATH="$PWD/root/lib/python$PYVER/site-packages:$OLDPPATH"
+if [ -e "$PWD/root/lib64" ]; then
+    echo "Detected 64bit install"
+    LIBDIR="lib64"
+else
+    LIBDIR="lib"
+fi
+
+export PYTHONPATH="$PWD/root/$LIBDIR/python$PYVER/site-packages:$OLDPPATH"
 export MANPATH="$PWD/root/share/man:$OLDMPATH"
 
 bin/canto-fetch -b
