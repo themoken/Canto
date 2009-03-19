33a926eb5dbe7fc008487c05381ec57922a35b31
Jack Miller <jack@codezen.org>
2009-03-19 13:10:15 -0500

    :retab

--- canto/canto.py
+++ canto/canto.py
@@ -354,23 +354,23 @@ class Main():
 
         self.cfg.stdscr = curses.initscr()
 
-	# curs_set can return ERR, we shouldn't care
-	try:
-	        curses.curs_set(0)
-	except:
-		pass
+        # curs_set can return ERR, we shouldn't care
+        try:
+                curses.curs_set(0)
+        except:
+                pass
 
-	# if any of these mess up though, the rest of the
-	# the operation is suspect, so die.
-	try:
-		curses.noecho()
-		curses.start_color()
-		curses.halfdelay(1)
-		curses.use_default_colors()
-	except:
-		self.cfg.log("Unable to init curses, bailing")
-		self.done()
-		
+        # if any of these mess up though, the rest of the
+        # the operation is suspect, so die.
+        try:
+                curses.noecho()
+                curses.start_color()
+                curses.halfdelay(1)
+                curses.use_default_colors()
+        except:
+                self.cfg.log("Unable to init curses, bailing")
+                self.done()
+
         self.resize = 0
         self.alarmed = 0
 
