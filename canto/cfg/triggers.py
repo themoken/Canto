09fd92e1d573cf40a7088206ae102c19f94ce5ba
Jack Miller <jack@codezen.org>
2009-05-26 20:19:06 -0500

    Trigger validation

--- canto/cfg/triggers.py
+++ canto/cfg/triggers.py
@@ -6,7 +6,10 @@ def post_parse(c):
     c.triggers = c.locals["triggers"]
 
 def validate(c):
-    pass
+    for t in c.triggers:
+        if t not in ["interval","signal","change_tag"]:
+            raise Exception, "%s is not a valid trigger name, try\
+                    \"interval\", \"signal\", or \"change_tag\""
 
 def test(c):
     pass
